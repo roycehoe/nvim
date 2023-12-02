d76544154a5839c31306ae4374c4511cab8c1d7d
roycehoe <royceinbox@hotmail.com>
2023-12-02 15:33:26 +0800

    chore: Savepoint successfully set up lsp zero, cleaning up code

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,64 +1,74 @@
-function configure_lsp_zero()
-	local lsp_zero = require('lsp-zero')
-
-	lsp_zero.on_attach(function(client, bufnr)
-		-- see :help lsp-zero-keybindings
-		-- to learn the available actions
-		lsp_zero.default_keymaps({buffer = bufnr})
-	end)
-
-end
-
-function configure_cmp()
-	local cmp = require('cmp')
-
-	local cmp_select = {behavior = cmp.SelectBehavior.Select}
-	local cmp_mappings = cmp.mapping.preset.insert({
-		['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-		['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-		['<Tab>'] = cmp.mapping.confirm({ select = true }),
-		["<C-Space>"] = cmp.mapping.complete(),
-	})
-	cmp.setup({
-		mapping = cmp_mappings
-	})
-end
-
 return {
 	{
-		{
-			'VonHeikemen/lsp-zero.nvim',
-			branch = 'v3.x',
-		},
-		{
-			'williamboman/mason.nvim',
-			config = true,
-		},
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		lazy = true,
+		config = false,
+		init = function()
+			vim.g.lsp_zero_extend_cmp = 0
+			vim.g.lsp_zero_extend_lspconfig = 0
+		end,
+	},
+	{
+		'williamboman/mason.nvim',
+		lazy = false,
+		config = true,
+	},
 
-		-- Autocompletion
-		{
-			'hrsh7th/nvim-cmp',
-			event = 'InsertEnter',
-			dependencies = {
-				{'L3MON4D3/LuaSnip'},
-			},
-			config = function()
-				configure_cmp()
-			end
+	-- Autocompletion
+	{
+		'hrsh7th/nvim-cmp',
+		event = 'InsertEnter',
+		dependencies = {
+			{'L3MON4D3/LuaSnip'},
 		},
+		config = function()
+			-- Here is where you configure the autocompletion settings.
+			local lsp_zero = require('lsp-zero')
+			lsp_zero.extend_cmp()
 
-		-- LSP
-		{
-			'neovim/nvim-lspconfig',
-			cmd = {'LspInfo', 'LspInstall', 'LspStart'},
-			event = {'BufReadPre', 'BufNewFile'},
-			dependencies = {
-				{'hrsh7th/cmp-nvim-lsp'},
-				{'williamboman/mason-lspconfig.nvim'},
-			},
-			config = function()
-				configure_lsp_zero()
-			end
-		}
+			local cmp = require('cmp')
+			local cmp_select = {behavior = cmp.SelectBehavior.Select}
+			local cmp_mappings = cmp.mapping.preset.insert({
+				['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+				['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+				['<Tab>'] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete(),
+			})
+			cmp.setup({
+				mapping = cmp_mappings
+			})
+		end
+	},
+
+	-- LSP
+	{
+		'neovim/nvim-lspconfig',
+		cmd = {'LspInfo', 'LspInstall', 'LspStart'},
+		event = {'BufReadPre', 'BufNewFile'},
+		dependencies = {
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'williamboman/mason-lspconfig.nvim'},
+		},
+		config = function()
+			-- This is where all the LSP shenanigans will live
+			local lsp_zero = require('lsp-zero')
+			lsp_zero.extend_lspconfig()
+			lsp_zero.on_attach(function(client, bufnr)
+				lsp_zero.default_keymaps({buffer = bufnr})
+			end)
+
+			require('mason-lspconfig').setup({
+				ensure_installed = {},
+				handlers = {
+					lsp_zero.default_setup,
+					lua_ls = function()
+						-- (Optional) Configure lua language server for neovim
+						local lua_opts = lsp_zero.nvim_lua_ls()
+						require('lspconfig').lua_ls.setup(lua_opts)
+					end,
+				}
+			})
+		end
 	}
 }
