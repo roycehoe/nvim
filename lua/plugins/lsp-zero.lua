d1d3ac932d89594309d997185f7579f4fbfea6a0
roycehoe <royceinbox@hotmail.com>
2023-12-02 15:04:08 +0800

    feat: setup lsp

---
+++ lua/plugins/lsp-zero.lua
@@ -0,0 +1,13 @@
+return {
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+
+	-- lsp support
+	{'neovim/nvim-lspconfig'},
+	-- Autocompletion
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'L3MON4D3/LuaSnip'},
+	-- language server manager
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'}
+}
