1fa84f5fdae87536bd1661e0f165ce5066c3c340
roycehoe <royceinbox@hotmail.com>
2023-12-02 15:21:00 +0800

    chore: Savepoint setting up lsp

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,13 +1,64 @@
-return {
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+function configure_lsp_zero()
+	local lsp_zero = require('lsp-zero')
 
-	-- lsp support
-	{'neovim/nvim-lspconfig'},
-	-- Autocompletion
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'L3MON4D3/LuaSnip'},
-	-- language server manager
-	{'williamboman/mason.nvim'},
-	{'williamboman/mason-lspconfig.nvim'}
+	lsp_zero.on_attach(function(client, bufnr)
+		-- see :help lsp-zero-keybindings
+		-- to learn the available actions
+		lsp_zero.default_keymaps({buffer = bufnr})
+	end)
+
+end
+
+function configure_cmp()
+	local cmp = require('cmp')
+
+	local cmp_select = {behavior = cmp.SelectBehavior.Select}
+	local cmp_mappings = cmp.mapping.preset.insert({
+		['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+		['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+		['<Tab>'] = cmp.mapping.confirm({ select = true }),
+		["<C-Space>"] = cmp.mapping.complete(),
+	})
+	cmp.setup({
+		mapping = cmp_mappings
+	})
+end
+
+return {
+	{
+		{
+			'VonHeikemen/lsp-zero.nvim',
+			branch = 'v3.x',
+		},
+		{
+			'williamboman/mason.nvim',
+			config = true,
+		},
+
+		-- Autocompletion
+		{
+			'hrsh7th/nvim-cmp',
+			event = 'InsertEnter',
+			dependencies = {
+				{'L3MON4D3/LuaSnip'},
+			},
+			config = function()
+				configure_cmp()
+			end
+		},
+
+		-- LSP
+		{
+			'neovim/nvim-lspconfig',
+			cmd = {'LspInfo', 'LspInstall', 'LspStart'},
+			event = {'BufReadPre', 'BufNewFile'},
+			dependencies = {
+				{'hrsh7th/cmp-nvim-lsp'},
+				{'williamboman/mason-lspconfig.nvim'},
+			},
+			config = function()
+				configure_lsp_zero()
+			end
+		}
+	}
 }
