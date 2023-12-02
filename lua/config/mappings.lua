5b94ef8a77ddeb86ab4d2f3e7ffde1480df97df8
roycehoe <royceinbox@hotmail.com>
2023-12-02 23:16:28 +0800

    feat: Install undotree

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -2,3 +2,4 @@ vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappin
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- [p]roject [v]iew
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git) --[g]et [s]tatus
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle) --[g]et [s]tatus
