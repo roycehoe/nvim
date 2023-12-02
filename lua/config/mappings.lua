f0c04c6fc4437604a61f0678166500f4cc25ce2c
roycehoe <royceinbox@hotmail.com>
2023-12-02 23:12:09 +0800

    feat: Move all mappings to mappings.lua file for easier reference

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- [p]roject [v]iew
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git) --[g]et [s]tatus
