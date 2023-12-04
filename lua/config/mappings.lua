e06b469037c3a96e4ea9cefa5340901354792a0e
royce <royceinbox@hotmail.com>
2023-12-04 21:50:38 +0800

    feat: Add mapping to show signature help

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -3,3 +3,6 @@ vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappin
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- [p]roject [v]iew
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git) --[g]et [s]tatus
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle) --[g]et [s]tatus
+
+vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help) --[s]ignature
+vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help) --[s]ignature
--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,58 +1,61 @@
 return {
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-	},
-	{
-		'williamboman/mason.nvim',
-		lazy = false,
-		config = true,
-	},
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+    },
+    {
+        'williamboman/mason.nvim',
+        lazy = false,
+        config = true,
+    },
 
-	-- Autocompletion
-	{
-		'hrsh7th/nvim-cmp',
-		event = 'InsertEnter',
-		config = function()
-			-- Here is where you configure the autocompletion settings.
-			local lsp_zero = require('lsp-zero')
-			lsp_zero.extend_cmp()
+    -- Autocompletion
+    {
+        'hrsh7th/nvim-cmp',
+        event = 'InsertEnter',
+        config = function()
+            -- Here is where you configure the autocompletion settings.
+            local lsp_zero = require('lsp-zero')
+            lsp_zero.extend_cmp()
 
-			local cmp = require('cmp')
-			local cmp_select = {behavior = cmp.SelectBehavior.Select}
-			local cmp_mappings = cmp.mapping.preset.insert({
-				['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-				['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-				['<Tab>'] = cmp.mapping.confirm({ select = true }),
-				["<C-Space>"] = cmp.mapping.complete(),
-			})
-			cmp.setup({
-				mapping = cmp_mappings
-			})
-		end
-	},
+            local cmp = require('cmp')
+            local cmp_select = {behavior = cmp.SelectBehavior.Select}
+            local cmp_mappings = cmp.mapping.preset.insert({
+                ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            })
+            cmp.setup({
+                mapping = cmp_mappings
+            })
+        end
+    },
 
-	-- LSP
-	{
-		'neovim/nvim-lspconfig',
-		dependencies = {
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'williamboman/mason-lspconfig.nvim'},
-		},
-		config = function()
-			-- This is where all the LSP shenanigans will live
-			local lsp_zero = require('lsp-zero')
-			lsp_zero.extend_lspconfig()
-			lsp_zero.on_attach(function(client, bufnr)
-				lsp_zero.default_keymaps({buffer = bufnr})
-			end)
+    -- LSP
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'williamboman/mason-lspconfig.nvim'},
+        },
+        opts = {
+            inlay_hints = { enabled = true },
+        },
+        config = function()
+            -- This is where all the LSP shenanigans will live
+            local lsp_zero = require('lsp-zero')
+            lsp_zero.extend_lspconfig()
+            lsp_zero.on_attach(function(client, bufnr)
+                lsp_zero.default_keymaps({buffer = bufnr})
+            end)
 
-			require('mason-lspconfig').setup({
-				ensure_installed = {},
-				handlers = {
-					lsp_zero.default_setup,
-				}
-			})
-		end
-	}
+            require('mason-lspconfig').setup({
+                ensure_installed = {},
+                handlers = {
+                    lsp_zero.default_setup,
+                }
+            })
+        end
+    }
 }
