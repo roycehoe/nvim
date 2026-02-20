11e49121bde7f777f8285463967ca355f3cc648f
royce <royceinbox@hotmail.com>
2026-02-20 13:59:58 +0800

    chore: savepoint

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,16 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function ()
-        local configs = require("nvim-treesitter.config")
-        configs.setup({
-            ensure_installed = {
-                "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "python", "go"
-            },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end
+    lazy = false,
+    -- config = function ()
+    --     local configs = require("nvim-treesitter.config")
+    --     configs.setup({
+    --         ensure_installed = {
+    --             "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "python", "go"
+    --         },
+    --         sync_install = false,
+    --         highlight = { enable = true },
+    --         indent = { enable = true },
+    --     })
+    -- end
 }
